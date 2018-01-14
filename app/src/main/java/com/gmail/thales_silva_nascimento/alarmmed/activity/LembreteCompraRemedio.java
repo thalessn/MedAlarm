5986994c3cbe44bbc64756b80b104135c0e30f02
ThalesXPS <thales.silva.nascimento@gmail.com>
2018-01-14 09:25:17 -0200

    Finalizado o layout do card lebrete

---
+++ app/src/main/java/com/gmail/thales_silva_nascimento/alarmmed/activity/LembreteCompraRemedio.java
@@ -0,0 +1,15 @@
+package com.gmail.thales_silva_nascimento.alarmmed.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gmail.thales_silva_nascimento.alarmmed.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
